5ef7d067562ded0f6437dab88f8cc864207066c8
Abdou BA <ba2664890@gmail.com>
2025-11-13 14:46:41 +0000

    ajout de login

--- app/main.py
+++ app/main.py
@@ -81,9 +81,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:5173",
         "https://backendappstageansd-production.up.railway.app",
+        "https://frontend-webscraping.vercel.app",
         "*"  # Pour le développement - à restreindre en production
     ],
     allow_credentials=True,
