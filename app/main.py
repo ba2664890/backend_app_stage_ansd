733664474517f79d6f83131e854f6da47af81014
Abdou Ba <ba2664890@gmail.com>
2025-12-29 13:42:09 +0000

    backV29

--- app/main.py
+++ app/main.py
@@ -207,7 +207,7 @@ ALLOWED_ORIGINS = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS,
+    allow_origins=["*"], # Allow all origins for development
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
