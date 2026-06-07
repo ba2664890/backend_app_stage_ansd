fd407b350a385801af2e2e30c67913981bae7857
Ton Nom <ton.email@exemple.com>
2026-06-07 14:06:26 +0000

    modification du design

--- app/services/cloudinary_service.py
+++ app/services/cloudinary_service.py
@@ -1,8 +1,28 @@
 import cloudinary
 import cloudinary.uploader
+from .config import settings
 
-from config import settings
+
 
 cloudinary.config(
     cloudinary_url=settings.CLOUDINARY_URL
 )
+
+class CloudinaryService:
+
+    @staticmethod
+    def upload_file(file):
+        result = cloudinary.uploader.upload(
+            file,
+            folder=settings.CLOUDINARY_FOLDER,
+            resource_type="auto"
+        )
+
+        return {
+            "url": result["secure_url"],
+            "public_id": result["public_id"]
+        }
+
+    @staticmethod
+    def delete_file(public_id: str):
+        return cloudinary.uploader.destroy(public_id)
